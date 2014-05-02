38a78ef7a837ffa6937a2b1722c3f7fe39703b65
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2014-05-02 17:18:17 +0100

    Added comments.

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Written in fairly standard Ruby (tested with 2.1); it should be enough to run
 Standalone executable in bin/run; can read the instruction either from file(s)
 specified as argument(s) to the script, or from the standard input; but beware
 of carriage returns when typing on the standard input.
+
+TODO: full coverage
+
+Copyright (c) Arthur Reutenauer, MIT/X11 licence, 30 April - 2 May 2014.
+BachoTeX, Kujawsko-Pomorskie Voivodeship, Poland.
--- src/mars_rover.rb
+++ src/mars_rover.rb
@@ -1,9 +1,19 @@
+# encoding: UTF-8
+
 module Mars
   ORIENTATIONS = ['N', 'W', 'S', 'E']
   XMAX = 50
   YMAX = 50
   MAXLINE = 100
 
+  # For methods that modify the state of an object, we use the convention that
+  # appending an exclamation mark means the object itself is modified; without
+  # the exclamation mark, the object is unaffected and a new object is
+  # returned.
+
+  # Orientation – North, West, etc.  Specified as a number of right-angle
+  # turns, anticlockwise, from North.
+  # Used to specify the orientation of the rover.
   class Orientation
     def initialize(letter)
       @angle = ORIENTATIONS.index(letter)
@@ -23,10 +33,13 @@ module Mars
       move
     end
 
+    # Rotate by the specified number of 90-degree turns (can be negative)
+    # Returns new object
     def rotate(n = 1)
       Orientation.new(ORIENTATIONS.index((@angle + n) % ORIENTATIONS.count))
     end
 
+    # Same thing, for self
     def rotate!(n = 1)
       @angle = (@angle + n) % ORIENTATIONS.count
     end
@@ -36,6 +49,7 @@ module Mars
     end
   end
 
+  # Points and vectors shared some of the logic; this is their base class
   class PointOrVector
     attr_reader :x, :y
 
@@ -57,6 +71,7 @@ module Mars
   end
 
   class Vector < PointOrVector
+    # Rotate by exactly 90 degrees anticlockwise
     def rotate
       Vector.new(-@y, @x)
     end
@@ -65,6 +80,7 @@ module Mars
       @x, @y = -@y, @x
     end
 
+    # Opposite vector
     def reverse
       Vector.new(-x, -y)
     end
@@ -74,6 +90,7 @@ module Mars
     end
   end
 
+  # Our grid.  Mostly used to keep track of “scents” (and to do some validations).
   class Grid
     attr_reader :xmax, :ymax
 
@@ -89,6 +106,8 @@ module Mars
       @scents = Hash.new
     end
 
+    # A position (or rather a robot at that position) is lost if it is, well,
+    # outside the grid
     def lost?(pos)
       pos.x < 0 || pos.y < 0 || pos.x > @xmax || pos.y > @ymax
     end
@@ -97,21 +116,30 @@ module Mars
       @scents[[position.x, position.y]] || []
     end
 
+    # A scent actually needs both a position and an orientation;
+    # in few cases (the four corners), more than one direction may be “cursed”,
+    # so we’re making that an array.
+    # We could use a Set from the standard library, but the interface is not
+    # that nice and Array does the job just as well.
     def set_scent(position, direction)
       p = [position.x, position.y]
       @scents[p] ||= []
       @scents[p] << direction
     end
 
+    # Here be dragons?
     def cursed?(position, direction)
       get_scents(position).include?(direction)
     end
   end
 
+  # We’re making one class that will execute the whole set of instructions; it
+  # is thus a group of rovers rather than a single one.  May change in the
+  # future.
   class RoverGroup
     def walk(instructions)
       locations = ""
-      lines = instructions.split("\n").reverse
+      lines = instructions.split("\n").reverse # reverse so that we can pop
       grid = Grid.new(lines.pop)
       while lines.count > 0 do
         start = lines.pop.split
